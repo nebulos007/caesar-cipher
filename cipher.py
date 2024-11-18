fc5f2f0fd0d93b189d90dc1218d31d70e74b0393
nebulos007 <carlos.stanton1@gmail.com>
2024-11-17 22:19:59 -0500

    Looks like we made it.

--- cipher.py
+++ cipher.py
@@ -1,5 +1,40 @@
 # add your code here
-cipher = {'a':'f', 'b':'g', 'c':'h'}
-userinput = input("Enter a phrase.")
+cipher = {
+    'a':'f',
+    'b':'g', 
+    'c':'h', 
+    "d": "i",
+    "e": "j",
+    "f": "k",
+    "g": "l",
+    "h": "m",
+    "i": "n",
+    "j": "o",
+    "k": "p",
+    "l": "q",
+    "m": "r",
+    "n": "s",
+    "o": "t",
+    "p": "u",
+    "q": "v",
+    "r": "w",
+    "s": "x",
+    "t": "y",
+    "u": "z",
+    "v": "a",
+    "w": "b",
+    "x": "c",
+    "y": "d",
+    "z": "e",
+}
+
+userinput = input("Enter the message to encrypt. ")
+userinput = userinput.lower()
+
+cipher_message = ""
 for letter in userinput:
-    print(cipher[letter])
+    if letter in cipher:
+        letter = cipher[letter]
+    cipher_message += letter
+   
+print("You entered: " + userinput + "\nThe encrypted message is: " + cipher_message)
